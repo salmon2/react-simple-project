74b0af1be4eae3265ccb95f2fbe03a28ff643a0f
park <parkkinam@vitruv.co.kr>
2022-05-19 15:07:16 +0900

    react-dom and redux setting complete

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+import { applyMiddleware, createStore, compose } from 'redux';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const logger = createLogger();
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(logger, ReduxThunk))
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
